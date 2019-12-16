5f83cfeee02f8421ae2d6f8ca2be48bf7c18c157
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2019-12-16 16:19:00 -0500

    Added navigation bar

---
+++ First-Nuxt-App/components/Nav/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav>
+        <ul>
+            <nuxt-link to="/" tag="li"> <a>HOME</a> </nuxt-link>
+            <nuxt-link to="/About" tag="li"> <a>ABOUT</a> </nuxt-link>
+            <nuxt-link to="/Contact" tag="li"></nuxt-link>
+        </ul>
+    </nav>
+</template>
